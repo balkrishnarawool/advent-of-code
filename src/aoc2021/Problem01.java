6ba099c76a22d381d1b75917751fffea427ec23f
Rawool <TS90XD@apm3lc02x60v3jgh8.home>
2021-12-02 00:28:55 +0100

    Refactored

--- src/aoc2021/Problem01.java
+++ src/aoc2021/Problem01.java
@@ -1,34 +1,31 @@
 package aoc2021;
 
-import aoc.Utils;
+import static aoc.Utils.fileToStringArray;
+import static aoc.Utils.stringToIntArray;
+
+import java.util.stream.IntStream;
 
 public class Problem01 {
 
-     static public int countIncreased(String[] strs) {
-        int[] ints = Utils.stringToIntArray(strs);
-        int n = 0;
-        for (int i = 1; i < strs.length; i++) {
-            if (ints[i-1] < ints[i]) n++;
-        }
-        return n;
+    static private long countIncreased(int[] ints) {
+        return IntStream.range(1, ints.length)
+                .filter(i -> ints[i-1] < ints[i])
+                .count();
     }
 
-    static public int countIncreasedSlidingWindow3(String[] strs) {
-        int[] ints = Utils.stringToIntArray(strs);
-        int n = 0;
-        for (int i = 3; i < strs.length; i++) {
-            if (ints[i-3] + ints[i-2] + ints[i-1] < ints[i-2] + ints[i-1] + ints[i]) n++;
-        }
-        return n;
+    static private long countIncreasedSlidingWindow3(int[] ints) {
+        return IntStream.range(3, ints.length)
+                .filter(i -> ints[i-3] + ints[i-2] + ints[i-1] < ints[i-2] + ints[i-1] + ints[i])
+                .count();
     }
 
     public static void main(String[] args) {
-        String[] input1 = Utils.fileToStringArray(Problem01.class, "Problem01Input1.txt");
-        String[] input2 = Utils.fileToStringArray(Problem01.class, "Problem01Input2.txt");
+        String[] input1 = fileToStringArray(Problem01.class, "Problem01Input1.txt");
+        String[] input2 = fileToStringArray(Problem01.class, "Problem01Input2.txt");
 
-        System.out.println(countIncreased(input1));
-        System.out.println(countIncreasedSlidingWindow3(input1));
-        System.out.println(countIncreased(input2));
-        System.out.println(countIncreasedSlidingWindow3(input2));
+        System.out.println(countIncreased(stringToIntArray(input1)));
+        System.out.println(countIncreasedSlidingWindow3(stringToIntArray(input1)));
+        System.out.println(countIncreased(stringToIntArray(input2)));
+        System.out.println(countIncreasedSlidingWindow3(stringToIntArray(input2)));
     }
 }
