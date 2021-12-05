5e7549ea084cc9ff619d4ea1c6d052ca56a5d71a
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2021-12-05 22:09:21 +0100

    Added maven, lombok and refactored a bit

--- src/aoc2021/Problem05.java
+++ src/aoc2021/Problem05.java
@@ -1,52 +1,52 @@
 package aoc2021;
 
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
 import static aoc.Utils.fileToStringArray;
+import static aoc.Utils.split;
 
 public class Problem05 {
 
+    @Value // Adds private final to all non-static fields
+    @AllArgsConstructor(staticName = "of")
+    private static class Point { int x; int y; }
+
     private static long countOverlapPoints(String[] strs, boolean diagonals) {
         int[][] arr = new int[1_000][1_000];
-        for (int i = 0; i < strs.length; i++) {
-            String str = strs[i];
-            String[] sa = str.split(" -> ");
-            String[] sa2 = sa[0].split(",");
-            String[] sa3 = sa[1].split(",");
-            int x1 = Integer.parseInt(sa2[0]);
-            int y1 = Integer.parseInt(sa2[1]);
-            int x2 = Integer.parseInt(sa3[0]);
-            int y2 = Integer.parseInt(sa3[1]);
-            if (x1 == x2) {
-                for (int j = Math.min(y1, y2); j <= Math.max(y1, y2); j++) {
-                    arr[x1][j]++;
+        for (String str: strs) {
+            String[] sa = split(str, ","," -> ", ",");
+            Point p1 = Point.of(Integer.parseInt(sa[0]), Integer.parseInt(sa[1]));
+            Point p2 = Point.of(Integer.parseInt(sa[2]), Integer.parseInt(sa[3]));
+            if (p1.x == p2.x) {
+                for (int j = Math.min(p1.y, p2.y); j <= Math.max(p1.y, p2.y); j++) {
+                    arr[p1.x][j]++;
                 }
             } else {
-                if (y1 == y2) {
-                    for (int j = Math.min(x1, x2); j <= Math.max(x1, x2); j++) {
-                        arr[j][y1]++;
+                if (p1.y == p2.y) {
+                    for (int j = Math.min(p1.x, p2.x); j <= Math.max(p1.x, p2.x); j++) {
+                        arr[j][p1.y]++;
                     }
                 } else {
                     if (diagonals) {
-                        if (x1 > x2) {
-                            int t = x1;
-                            x1 = x2;
-                            x2 = t;
-                            t = y1;
-                            y1 = y2;
-                            y2 = t;
+                        if (p1.x > p2.x) {
+                            Point t = p1;
+                            p1 = p2;
+                            p2 = t;
                         }
-                        for (int j = 0; j <= x2-x1; j++) {
-                            if (y1 < y2) {
-                                arr[x1+j][y1+j]++;
+                        for (int j = 0; j <= p2.x-p1.x; j++) {
+                            if (p1.y < p2.y) {
+                                arr[p1.x+j][p1.y+j]++;
                             } else {
-                                arr[x1+j][y1-j]++;
+                                arr[p1.x+j][p1.y-j]++;
                             }
                         }
                     }
                 }
             }
         }
-        int c = 0;
 
+        int c = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (arr[i][j] >= 2) c++;
