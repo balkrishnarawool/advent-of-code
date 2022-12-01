a959b3628279cc623884549a14be617dc2da6783
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2022-12-01 09:34:35 +0100

    Day01 refactored

--- src/aoc2022/Problem01.java
+++ src/aoc2022/Problem01.java
@@ -3,30 +3,37 @@ package aoc2022;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.stream.IntStream;
-
-import static aoc.Utils.fileToStringArray;
-import static aoc.Utils.stringToIntArray;
+import java.util.List;
 
 public class Problem01 {
 
     public static void main(String[] args) throws IOException {
-        solvePart1();
-        solvePart2();
+        solvePart1("./src/aoc2022/Problem01Input1.txt");
+        solvePart1("./src/aoc2022/Problem01Input2.txt");
+        solvePart2("./src/aoc2022/Problem01Input1.txt");
+        solvePart2("./src/aoc2022/Problem01Input2.txt");
     }
 
-    private static void solvePart2() throws IOException {
+    private static void solvePart1(String path) throws IOException {
+        var list = getSummedIntsFromFile(path);
+        System.out.println(list.stream().max(Comparator.comparingInt(e -> e)));
+    }
+
+    private static void solvePart2(String path) throws IOException {
+        var list = getSummedIntsFromFile(path);
+        System.out.println(list.stream().sorted(Comparator.reverseOrder()).limit(3).reduce(Integer::sum));
+    }
+
+    private static List<Integer> getSummedIntsFromFile(String path) throws IOException {
         var list = new ArrayList<Integer>();
         var sum = new MutableInt(0);
-        try(var lines = Files.lines(Path.of("/Users/TS90XD/dev/java/aoc/advent-of-code/src/aoc2022/Problem01Input2.txt"))) {
+        try(var lines = Files.lines(Path.of(path))) {
             lines.forEach(s -> {
                 if (s.length() == 0) {
                     list.add(sum.getV());
@@ -37,25 +44,7 @@ public class Problem01 {
             });
             // Add last element
             list.add(sum.getV());
-            System.out.println(list.stream().sorted(Comparator.reverseOrder()).limit(3).reduce(Integer::sum));
-        }
-    }
-
-    private static void solvePart1() throws IOException {
-        var list = new ArrayList<Integer>();
-        var sum = new MutableInt(0);
-        try(var lines = Files.lines(Path.of("/Users/TS90XD/dev/java/aoc/advent-of-code/src/aoc2022/Problem01Input1.txt"))) {
-            lines.forEach(s -> {
-                if (s.length() == 0) {
-                    list.add(sum.getV());
-                    sum.setV(0);
-                } else {
-                    sum.setV(sum.getV() + Integer.parseInt(s));
-                }
-            });
-            // Add last element
-            list.add(sum.getV());
-            System.out.println(list.stream().max(Comparator.comparingInt(e -> e)));
+            return list;
         }
     }
 
@@ -65,4 +54,10 @@ public class Problem01 {
     static class MutableInt {
         int v;
     }
+    
+//     Output
+//    Optional[24000]
+//    Optional[67027]
+//    Optional[45000]
+//    Optional[197291]
 }
