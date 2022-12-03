9a0cbda6bcae0c8ba305d42dbeb2d4fc2666f2ab
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2022-12-03 05:57:00 +0100

    Day03 template

---
+++ src/aoc2022/Problem03.java
@@ -0,0 +1,33 @@
+package aoc2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Problem03 {
+
+    public static void main(String[] args) throws IOException {
+        solvePart1("./src/aoc2022/Problem02Input1.txt");
+        solvePart1("./src/aoc2022/Problem02Input2.txt");
+        solvePart2("./src/aoc2022/Problem02Input1.txt");
+        solvePart2("./src/aoc2022/Problem02Input2.txt");
+    }
+
+    private static void solvePart1(String path) throws IOException {
+    }
+
+    private static void solvePart2(String path) throws IOException {
+    }
+
+    private static void solve(String path, Map<String, Integer> map) throws IOException {
+        try(var lines = Files.lines(Path.of(path))) {
+
+        }
+    }
+
+//     Output
+
+
+}
