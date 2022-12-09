6572e1d74541f3dca1f9021d02f69efd5d7d3a5a
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2022-12-09 18:20:44 +0100

    Day 09 refactored

--- src/aoc2022/Problem09.java
+++ src/aoc2022/Problem09.java
@@ -31,6 +31,7 @@ public class Problem09 {
         set.add(s);
         var h = s;
         var t = s;
+
         for (var str: list) {
             var sa = str.split(" ");
             var n = Integer.parseInt(sa[1]);
@@ -39,16 +40,8 @@ public class Problem09 {
                 case "R":
                     for (int i = 0; i < n; i++) {
                         if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x+1, t.y+1); }
-                        else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-
                         else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x+1, t.y); }
-                        else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
                         else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x+1, t.y-1); }
-                        else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
 
                         h = new Coords(h.x+1, h.y);
                         set.add(t);
@@ -56,16 +49,8 @@ public class Problem09 {
                     break;
                 case "L":
                     for (int i = 0; i < n; i++) {
-                        if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x-1, t.y+1); }
-
-                        else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
+                        if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x-1, t.y+1); }
                         else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x-1, t.y); }
-
-                        else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
                         else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x-1, t.y-1); }
 
                         h = new Coords(h.x-1, h.y);
@@ -74,15 +59,7 @@ public class Problem09 {
                 break;
                 case "U":
                     for (int i = 0; i < n; i++) {
-                        if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-
-                        else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
-                        else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x+1, t.y-1); }
+                        if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x+1, t.y-1); }
                         else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y-1); }
                         else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x-1, t.y-1); }
 
@@ -95,14 +72,6 @@ public class Problem09 {
                         if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x+1, t.y+1); }
                         else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y+1); }
                         else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x-1, t.y+1); }
-
-                        else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
-                        else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-                        else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
 
                         h = new Coords(h.x, h.y+1);
                         set.add(t);
@@ -121,6 +90,7 @@ public class Problem09 {
         var h = s;
         var ts = new Coords[9];
         Arrays.fill(ts, s);
+
         for (var str: list) {
             var sa = str.split(" ");
             var n = Integer.parseInt(sa[1]);
@@ -132,96 +102,69 @@ public class Problem09 {
         return set.size();
     }
 
-    private static Coords move(String dir, Coords h, Coords[] ts) {
-        var t = new Coords(ts[0].x, ts[0].y);
-        if (dir.equals("R")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x+1, t.y+1); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
+    private static Coords move(String dir, Coords head, Coords[] knots) {
+        var t = new Coords(knots[0].x, knots[0].y); //temporary
+        switch (dir) {
+            case "R":
+                if (t.x == head.x-1 && t.y == head.y-1) { t = new Coords(t.x+1, t.y+1); }
+                else if (t.x == head.x-1 && t.y == head.y) { t = new Coords(t.x+1, t.y); }
+                else if (t.x == head.x-1 && t.y == head.y+1) { t = new Coords(t.x+1, t.y-1); }
 
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x+1, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
+                moveEachKnot(t, knots);
+                head = new Coords(head.x+1, head.y);
+                break;
+            case "L":
+                if (t.x == head.x+1 && t.y == head.y-1) { t = new Coords(t.x-1, t.y+1); }
+                else if (t.x == head.x+1 && t.y == head.y) { t = new Coords(t.x-1, t.y); }
+                else if (t.x == head.x+1 && t.y == head.y+1) { t = new Coords(t.x-1, t.y-1); }
 
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x+1, t.y-1); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
+                moveEachKnot(t, knots);
+                head = new Coords(head.x-1, head.y);
+                break;
+            case "U":
+                if (t.x == head.x-1 && t.y == head.y+1) { t = new Coords(t.x+1, t.y-1); }
+                else if (t.x == head.x && t.y == head.y+1) { t = new Coords(t.x, t.y-1); }
+                else if (t.x == head.x+1 && t.y == head.y+1) { t = new Coords(t.x-1, t.y-1); }
 
-            moveEachKnot(t, ts);
-            h = new Coords(h.x+1, h.y);
+                moveEachKnot(t, knots);
+                head = new Coords(head.x, head.y-1);
+                break;
+            case "D":
+                if (t.x == head.x-1 && t.y == head.y-1) { t = new Coords(t.x+1, t.y+1); }
+                else if (t.x == head.x && t.y == head.y-1) { t = new Coords(t.x, t.y+1); }
+                else if (t.x == head.x+1 && t.y == head.y-1) { t = new Coords(t.x-1, t.y+1); }
+
+                moveEachKnot(t, knots);
+                head = new Coords(head.x, head.y+1);
+                break;
         }
-        else if (dir.equals("L")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x-1, t.y+1); }
-
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x-1, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x-1, t.y-1); }
-
-            moveEachKnot(t, ts);
-            h = new Coords(h.x-1, h.y);
-        }
-        else if (dir.equals("U")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x+1, t.y-1); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y-1); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x-1, t.y-1); }
-
-            moveEachKnot(t, ts);
-            h = new Coords(h.x, h.y-1);
-        }
-        else if (dir.equals("D")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x+1, t.y+1); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y+1); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x-1, t.y+1); }
-
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-
-            moveEachKnot(t, ts);
-            h = new Coords(h.x, h.y+1);
-        }
-        return h;
+        return head;
     }
 
     private static void moveEachKnot(Coords newPos, Coords[] knots) {
-        var tempPos = newPos;
+        var t = newPos; //temporary
         for (int i = 0; i < knots.length; i++) {
-            newPos = tempPos;
+            newPos = t;
+            // if no need to move, break
             if (knots[i].x == newPos.x && knots[i].y == newPos.y) break;
+
             if (i < knots.length - 1) {
-                // compare newPos, knots[i] and knots[i+1] and create tempPos
+                // compare newPos, knots[i] and knots[i+1] and create t
                 // for adjacent, no move
-                if (adjacentOrSame(newPos, knots[i + 1])) tempPos = copy(knots[i + 1]);
-                else
-                    // for R, L, U, D -> use move2
-                    if (isR(knots[i], newPos)) tempPos = move2("R", copy(knots[i]), copy(knots[i + 1]));
-                    else if (isL(knots[i], newPos)) tempPos = move2("L", copy(knots[i]), copy(knots[i + 1]));
-                    else if (isU(knots[i], newPos)) tempPos = move2("U", copy(knots[i]), copy(knots[i + 1]));
-                    else if (isD(knots[i], newPos)) tempPos = move2("D", copy(knots[i]), copy(knots[i + 1]));
-                    else
-                        // if vertically or horizontally on same line then x+/-1 or y+/-1
-                        if (newPos.x == knots[i + 1].x) tempPos = (newPos.y == knots[i + 1].y + 2) ? new Coords(newPos.x, newPos.y - 1) : new Coords(newPos.x, newPos.y + 1);
-                        else if (newPos.y == knots[i + 1].y) tempPos = (newPos.x == knots[i + 1].x + 2) ? new Coords(newPos.x - 1, newPos.y) : new Coords(newPos.x + 1, newPos.y);
-                        // else, they are on diagonal, so move diagonally accordingly
-                        else tempPos = new Coords(knots[i + 1].x + newPos.x - knots[i].x, knots[i + 1].y + newPos.y - knots[i].y);
+                if (adjacentOrSame(newPos, knots[i + 1])) t = copy(knots[i + 1]);
+
+                // if R, L, U, D
+                else if (isR(knots[i], newPos)) t = moveKnots("R", copy(knots[i]), copy(knots[i + 1]));
+                else if (isL(knots[i], newPos)) t = moveKnots("L", copy(knots[i]), copy(knots[i + 1]));
+                else if (isU(knots[i], newPos)) t = moveKnots("U", copy(knots[i]), copy(knots[i + 1]));
+                else if (isD(knots[i], newPos)) t = moveKnots("D", copy(knots[i]), copy(knots[i + 1]));
+
+                // if vertically or horizontally on same line then x+/-1 or y+/-1
+                else if (newPos.x == knots[i + 1].x) t = (newPos.y == knots[i + 1].y + 2) ? new Coords(newPos.x, newPos.y - 1) : new Coords(newPos.x, newPos.y + 1);
+                else if (newPos.y == knots[i + 1].y) t = (newPos.x == knots[i + 1].x + 2) ? new Coords(newPos.x - 1, newPos.y) : new Coords(newPos.x + 1, newPos.y);
+
+                // else, they are on diagonal, so move diagonally accordingly
+                else t = new Coords(knots[i + 1].x + newPos.x - knots[i].x, knots[i + 1].y + newPos.y - knots[i].y);
             }
             knots[i] = newPos;
         }
@@ -259,73 +202,39 @@ public class Problem09 {
                (a.x == b.x-1 && a.y == b.y-1);
     }
 
-    private static Coords move2(String dir, Coords h, Coords t) {
-        if (dir.equals("R")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x+1, t.y+1); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x+1, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x+1, t.y-1); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-
+    private static Coords moveKnots(String dir, Coords k1, Coords k2) { // k1 = knot1, k2 = knot2
+        var temp = new Coords(k2.x, k2.y); //temporary
+        switch (dir) {
+            case "R":
+                if (k2.x == k1.x-1 && k2.y == k1.y-1) { temp = new Coords(temp.x+1, temp.y+1); }
+                else if (k2.x == k1.x-1 && k2.y == k1.y) { temp = new Coords(temp.x+1, temp.y); }
+                else if (k2.x == k1.x-1 && k2.y == k1.y+1) { temp = new Coords(temp.x+1, temp.y-1); }
+            break;
+            case "L":
+                if (k2.x == k1.x+1 && k2.y == k1.y-1) { temp = new Coords(temp.x-1, temp.y+1); }
+                else if (k2.x == k1.x+1 && k2.y == k1.y) { temp = new Coords(temp.x-1, temp.y); }
+                else if (k2.x == k1.x+1 && k2.y == k1.y+1) { temp = new Coords(temp.x-1, temp.y-1); }
+            break;
+            case "U":
+                if (k2.x == k1.x-1 && k2.y == k1.y+1) { temp = new Coords(temp.x+1, temp.y-1); }
+                else if (k2.x == k1.x && k2.y == k1.y+1) { temp = new Coords(temp.x, temp.y-1); }
+                else if (k2.x == k1.x+1 && k2.y == k1.y+1) { temp = new Coords(temp.x-1, temp.y-1); }
+            break;
+            case "D":
+                if (k2.x == k1.x-1 && k2.y == k1.y-1) { temp = new Coords(temp.x+1, temp.y+1); }
+                else if (k2.x == k1.x && k2.y == k1.y-1) { temp = new Coords(temp.x, temp.y+1); }
+                else if (k2.x == k1.x+1 && k2.y == k1.y-1) { temp = new Coords(temp.x-1, temp.y+1); }
         }
-        else if (dir.equals("L")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x-1, t.y+1); }
-
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x-1, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x-1, t.y-1); }
-
-        }
-        else if (dir.equals("U")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x+1, t.y-1); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y-1); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x-1, t.y-1); }
-
-        }
-        else if (dir.equals("D")) {
-            if (t.x == h.x-1 && t.y == h.y-1) { t = new Coords(t.x+1, t.y+1); }
-            else if (t.x == h.x && t.y == h.y-1) { t = new Coords(t.x, t.y+1); }
-            else if (t.x == h.x+1 && t.y == h.y-1) { t = new Coords(t.x-1, t.y+1); }
-
-            else if (t.x == h.x-1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y) { t = new Coords(t.x, t.y); }
-
-            else if (t.x == h.x-1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-            else if (t.x == h.x+1 && t.y == h.y+1) { t = new Coords(t.x, t.y); }
-
-        }
-        return t;
+        return temp;
     }
-
-
+    
     record Coords(int x, int y) { }
 
 //     Output
-//     CMZ
-//     QPJPLMNNR
-//     MCD
-//     BQDNWJPVJ
+//     13
+//     6266
+//     1
+//     36
+//     2369
 
 }
