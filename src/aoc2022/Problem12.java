39a51b0922aad4a5c1fd13071ad3e3b0da07e39e
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2022-12-12 13:05:32 +0100

    Day 12

--- src/aoc2022/Problem12.java
+++ src/aoc2022/Problem12.java
@@ -25,35 +25,69 @@ public class Problem12 {
     }
 
     private static void solvePart2(String path) throws IOException {
-//        System.out.println(solve2(path));
+        System.out.println(solve2(path));
     }
 
     private static int solve(String path) throws IOException {
         var map = createMap(readFile(path));
-        return getShortestDist(map);
+        return getShortestDist(map.s, map);
+    }
+
+    private static int solve2(String path) throws IOException {
+        var map = createMap(readFile(path));
+        var as = getAllAs(map);
+        int min = Integer.MAX_VALUE;
+        for (var a: as) {
+            var d = getShortestDist(a, map);
+            if (d < min) { min = d; }
+        }
+        return min;
+    }
+
+    private static List<Cell> getAllAs(Map map) {
+        var as = new ArrayList<Cell>();
+        for (int i = 0; i < map.grid.length; i++) {
+            for (int j = 0; j < map.grid[i].length; j++) {
+                if (map.grid[i][j] == 0) as.add(new Cell(i, j));
+            }
+        }
+        return as;
     }
 
     private static Map createMap(List<String> lines) {
         var grid = new int[lines.size()][];
-        Cell s = null;
-        Cell e = null;
+        var s = findCell(lines, 'S');
+        var e = findCell(lines, 'E');
         for (int i = 0; i < lines.size(); i++) {
             var str = lines.get(i);
             grid[i] = new int[str.length()];
             var a = str.toCharArray();
             for (int j = 0; j < lines.get(i).length(); j++) {
-                if (a[j] == 'S') { s = new Cell(i, j); a[j] = 'a'; }
-                if (a[j] == 'E') { e = new Cell(i, j); a[j] = 'z'; }
+                if (a[j] == 'S') { a[j] = 'a'; }
+                if (a[j] == 'E') { a[j] = 'z'; }
                 grid[i][j] = a[j] - 'a';
             }
         }
         return new Map(s, e, grid);
     }
 
-    private static int getShortestDist(Map map) {
-        var q = new PriorityQueue<Entry>();
-        q.add(new Entry(map.s, 0));
+    private static Cell findCell(List<String> lines, char c) {
+        for (int i = 0; i < lines.size(); i++) {
+            var str = lines.get(i);
+            var a = str.toCharArray();
+            for (int j = 0; j < lines.get(i).length(); j++) {
+                if (a[j] == c) return new Cell(i, j);
+            }
+        }
+        throw new RuntimeException("Cell with content "+ c +" not found");
+    }
+
+    private static int getShortestDist(Cell a, Map map) {
+        var q = new PrioQueue();
+        q.add(new Entry(a, 0));
+
         var visited = new HashSet<Cell>();
+
         while (!q.isEmpty()) {
             var e = q.remove();
 
@@ -61,8 +95,7 @@ public class Problem12 {
                 var u = new Cell(e.c.r - 1, e.c.c); // up
                 if (!visited.contains(u) && map.grid[u.r][u.c] <= map.grid[e.c.r][e.c.c] + 1) {
                     if (u.equals(map.e)) { return e.d + 1; }
-                    var newE = new Entry(u, e.d + 1);
-                    addIfNeeded(q, newE);
+                    q.add(new Entry(u, e.d + 1));
                 }
             }
 
@@ -70,8 +103,7 @@ public class Problem12 {
                 var d = new Cell(e.c.r + 1, e.c.c); // down
                 if (!visited.contains(d) && map.grid[d.r][d.c] <= map.grid[e.c.r][e.c.c] + 1) {
                     if (d.equals(map.e)) { return e.d + 1; }
-                    var newE = new Entry(d, e.d + 1);
-                    addIfNeeded(q, newE);
+                    q.add(new Entry(d, e.d + 1));
                 }
             }
 
@@ -79,8 +111,7 @@ public class Problem12 {
                 var l = new Cell(e.c.r, e.c.c - 1); // left
                 if (!visited.contains(l) && map.grid[l.r][l.c] <= map.grid[e.c.r][e.c.c] + 1) {
                     if (l.equals(map.e)) { return e.d + 1; }
-                    var newE = new Entry(l, e.d + 1);
-                    addIfNeeded(q, newE);
+                    q.add(new Entry(l, e.d + 1));
                 }
             }
 
@@ -88,27 +119,28 @@ public class Problem12 {
                 var r = new Cell(e.c.r, e.c.c + 1); // right
                 if (!visited.contains(r) && map.grid[r.r][r.c] <= map.grid[e.c.r][e.c.c] + 1) {
                     if (r.equals(map.e)) { return e.d + 1; }
-                    var newE = new Entry(r, e.d + 1);
-                    addIfNeeded(q, newE);
+                    q.add(new Entry(r, e.d + 1));
                 }
             }
 
             visited.add(e.c);
         }
-        return 0;
+        return Integer.MAX_VALUE;
     }
 
-    private static void addIfNeeded(PriorityQueue<Entry> q, Entry newE) {
-        var removeEs = new ArrayList<Entry>();
-        for (var e: q) {
-            if (e.c.r == newE.c.r && e.c.c == newE.c.c) {
-                if (e.d >= newE.d) {
-                    removeEs.add(e);
+    static class PrioQueue extends PriorityQueue<Entry> {
+        @Override
+        public boolean add(Entry newE) {
+            for (var e: this) {
+                if (e.c.r == newE.c.r && e.c.c == newE.c.c) {
+                    if (e.d >= newE.d) {
+                        remove(e);
+                        break; // Max one 'bigger' entry possible. If found, remove and break loop.
+                    }
                 }
             }
+            return super.add(newE);
         }
-        removeEs.forEach(q::remove);
-        q.add(newE);
     }
 
     record Map (Cell s, Cell e, int[][] grid ) { } // start and end
@@ -123,5 +155,7 @@ public class Problem12 {
 //     Output
 //     31
 //     449
+//     29
+//     443
 
 }
