d50181a3f5ec72e76f91665fcd6a9dbef621f3e3
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2022-12-15 09:39:27 +0100

    Day 15 - template - also commiting Day 14 solution, forgot to commit yesterday

---
+++ src/aoc2022/Problem15.java
@@ -0,0 +1,31 @@
+package aoc2022;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Problem15 {
+
+    public static void main(String[] args) throws IOException {
+        solvePart1("./src/aoc2022/Problem15Input1.txt");
+//        solvePart1("./src/aoc2022/Problem15Input2.txt");
+//        solvePart2("./src/aoc2022/Problem15Input1.txt");
+//        solvePart2("./src/aoc2022/Problem15Input2.txt");
+    }
+
+    private static void solvePart1(String path) throws IOException {
+        System.out.println(solve(path));
+    }
+
+    private static void solvePart2(String path) throws IOException {
+//        System.out.println(solve2(path));
+    }
+
+    private static long solve(String path) throws IOException {
+        return 0;
+    }
+
+//     Output
+
+
+}
