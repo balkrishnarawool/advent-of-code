d6d6832eeeccbdc53c1653c9d523cefe14425108
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2023-12-09 21:51:55 +0100

    Day 09

---
+++ src/aoc2023/Problem09A.java
@@ -0,0 +1,64 @@
+package aoc2023;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem09A {
+    public static void main(String[] args) {
+//        var str = Problem09Input.INPUT01;
+        var str = Problem09Input.INPUT02;
+//        var str = Problem09Input.INPUT03;
+
+        System.out.println(
+                str.lines()
+                        .map(Problem09A::calculateNextValue)
+                        .reduce(Integer::sum)
+                        .orElse(0)
+        );
+    }
+
+    private static Integer calculateNextValue(String s) {
+        var seq = parse(s);
+        var seqList = createSeqList(seq);
+        return getNextValue(seqList);
+    }
+
+    private static List<Integer> parse(String s) {
+        return Arrays.stream(s.split("\\s+")).map(Integer::parseInt).toList();
+    }
+
+    private static List<List<Integer>> createSeqList(List<Integer> seq) {
+        var seqList = new ArrayList<List<Integer>>();
+        var curSeq = seq;
+        while(!allZero(curSeq)) {
+            seqList.add(curSeq);
+            curSeq = generateNextSeq(curSeq);
+       }
+        return seqList;
+    }
+
+    private static List<Integer> generateNextSeq(List<Integer> curSeq) {
+        var nextSeq = new ArrayList<Integer>();
+        for (int i = 0; i < curSeq.size()-1; i++) {
+            nextSeq.add(curSeq.get(i+1) - curSeq.get(i));
+        }
+        return nextSeq;
+    }
+
+    private static boolean allZero(List<Integer> seq) {
+        for (int num: seq) {
+            if (num != 0) return false;
+        }
+        return true;
+    }
+
+    private static int getNextValue(List<List<Integer>> seqList) {
+        var value = 0;
+        for (int i = seqList.size()-1; i >= 0 ; i--) {
+            value += seqList.get(i).get(seqList.get(i).size()-1);
+        }
+        return value;
+    }
+
+}
--- src/aoc2023/Problem10.java
+++ src/aoc2023/Problem10.java
@@ -1,6 +1,6 @@
 package aoc2023;
 
-public class Problem09 {
+public class Problem10 {
     public static void main(String[] args) {
         var str = Problem03Input.INPUT01;
 //        var str = Problem03Input.INPUT02;
@@ -8,7 +8,7 @@ public class Problem09 {
 
         System.out.println(
                 str.lines()
-                        .map(Problem09::solve)
+                        .map(Problem10::solve)
         );
     }
 
